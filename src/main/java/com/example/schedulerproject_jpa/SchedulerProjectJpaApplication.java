f9f7e68e45d7603543b50dbb431e8a34734865d1
giwon-wg <giwon8766@naver.com>
2025-03-31 15:21:37 +0900

    chore: 프로젝트 초기 세팅 및 Swagger 설정 완료
    - Spring Boot 3.4.4 + Corretto-17
    - 의존성 설정 (JPA, Validation, Swagger 등)
    - application.yml 설정
    - Swagger 2.6.0 정상 작동 확인

---
+++ src/main/java/com/example/schedulerproject_jpa/SchedulerProjectJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.schedulerproject_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulerProjectJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchedulerProjectJpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/schedulerproject_jpa/SchedulerProjectJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.schedulerproject_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulerProjectJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

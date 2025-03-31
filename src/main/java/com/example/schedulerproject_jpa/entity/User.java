6f6cb9f7946c1284e13b7cff4f0234c37b19e51d
giwon-wg <giwon8766@naver.com>
2025-03-31 19:15:51 +0900

    feat: User 엔티티 생성 및 JPA Auditing 설정

--- src/main/java/com/example/schedulerproject_jpa/entity/User.java
+++ src/main/java/com/example/schedulerproject_jpa/entity/User.java
@@ -1,11 +1,8 @@
 package com.example.schedulerproject_jpa.entity;
 
-
 import jakarta.persistence.*;
-import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -16,39 +13,23 @@ import java.time.LocalDateTime;
 @Getter
 @NoArgsConstructor
 @EntityListeners(AuditingEntityListener.class)
-public class Schedule {
+public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
-    private String user;
+    @Column(nullable = false, length = 10)
+    private String userName;
 
-    @Column(nullable = false, length = 50)
-    private String title;
+    @Column(nullable = false, unique = true)
+    private String email;
 
-    @Column(nullable = false, columnDefinition = "TEXT", length = 200)
-    private String todo;
-
-    @Setter(AccessLevel.NONE)
     @CreatedDate
     @Column(updatable = false)
     private LocalDateTime createdAt;
 
-    @Setter(AccessLevel.NONE)
     @LastModifiedDate
     private LocalDateTime modifiedAt;
 
-    public Schedule(String user, String title, String todo){
-        this.user = user;
-        this.title = title;
-        this.todo = todo;
-    }
-
-    public void update(String user, String title, String todo){
-        this.user = user;
-        this.title = title;
-        this.todo = todo;
-    }
 }
